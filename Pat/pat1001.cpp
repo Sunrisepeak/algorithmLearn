f6dbfb4dc59ed11a6d61b65448ba3cd545f6fc4e
Speak <1272533009@qq.com>
2018-10-22 13:44:48 +0800

    pat1002

--- Pat/pat1001.cpp
+++ Pat/pat1001.cpp
@@ -1,32 +1,32 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
 int main()
 {
-  int a, b, c[10],flag;
+  int a, b, c[10],top;
   while(cin >> a >> b)
   {
-	flag = 0;
+	top = -1;			//初始化top
 	a = a + b;
-	if(a < 0)
+	if(a < 0)			//取a+b和的绝对值
 	{
 		cout << "-";
 		a = -a;
 	}
-	if(a == 0)
+	if(a == 0)			//和为0的情况
 		cout << "0";
-	while(a)
+	while(a)			//对和进行求余逐个入栈
 	{
-		c[flag++] = a % 10;
+		c[++top] = a % 10;
 		a = a/10;
 	}
-	int k = flag%3;
-	for(int i = flag -1; i >= 0; i--)
+	int length = top + 1;
+	int k = length%3;	//用于记录第一逗号的标记（例如是五位数会在第二位输出，4位数是第一个输出）
+	while(top >= 0)		//循环出栈
 	{
-		cout << c[i];
-		if(k && (flag - i > 3) && flag - i == k)
-			cout << ",";
-		if((flag-i - k)%3 == 0 && i  > 0)
-			cout << ",";
+		cout << c[top--];	
+		if(length > 3 && top >= 2)		//判断和小于等于三位数不用输出逗号，和确保最后一个逗号输出是在倒数第三个数输出时输出
+			if(length - top - 1== k || ((length - top - k - 1)%3 == 0))	//左边是判断第一个逗号输出，右边是判断剩余逗号输出(就是对3求余)
+				cout << ",";
 	}
 	cout << "\n";
   }
---
+++ Pat/pat1002.cpp
@@ -0,0 +1,34 @@
+﻿#include<iomanip>　　　　//cout输出格式控制
+using namespace std;
+double a[1001];
+int main()
+{
+    int n1, n2, flag;
+    memset(a, 0,sizeof(a));
+    cin >> n1;
+    for(int i = 0; i < n1; i++)
+    {
+        cin >> flag;
+        cin >> a[flag];
+    }
+    cin >> n2;
+    double x;
+    for(int i = 0; i < n2; i++)
+    {
+        cin >> flag;
+        cin >> x;
+        if(a[flag] != 0)　　　　　　　　　　　　　　//减去A中存在项
+            n1--;
+        a[flag] += x;
+        if(a[flag] == 0)
+            n1--;　　　　　　                   	//减去和为0的项
+        
+    }
+    cout << n1+n2;　　　　　　　　　　　　　　　　　//输出总的非0项，若多项式之和为0，这里输出一个0即可
+    for(int i = 1000; i >= 0; i--)
+    {
+        if(a[i] != 0)
+            cout << " " << i << " " << fixed << setprecision(1)<< a[i];　　　　//用fixed + setprecision(x),控制保留x位小数
+    }
+    return 0;
+}
