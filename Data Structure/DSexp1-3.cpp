27483ded6cf30600b0e7b458151ede0f90716bf4
Speak <1272533009@qq.com>
2019-02-20 00:45:26 +0800

    add optimize function about prime_number

--- Data Structure/DSexp1-3.cpp
+++ Data Structure/DSexp1-3.cpp
@@ -15,6 +15,35 @@ bool Isprime_method2(int n)
             return false;
     return true;
 }
+//by stackoverflow
+int is_prime(long num)
+{
+    int k = 1, a = 0, b = 0;
+    long sr;
+    switch(num)
+        {
+        case 1: return 0;
+        case 2: return 1;
+        case 3: return 1;
+        case 4: return 0;
+        case 5: return 1;
+        case 6: return 0;
+        case 7: return 1;
+    }
+    if (num % 2 == 0) return 0;
+    if (num % 3 == 0) return 0;
+    sr = (int) sqrt(num);
+    while (b < sr) {
+        a = (6 * k) - 1;
+        b = (6 * k) + 1;
+        if (num % a == 0)
+            return 0;
+        if (num % b == 0)
+            return 0;
+        k += 1;
+    }
+    return 1;
+}
 void ptime(clock_t begin, clock_t end)
 {
     std::cout << "test case of time is " << (end - begin) * 1.0 /CLOCKS_PER_SEC
